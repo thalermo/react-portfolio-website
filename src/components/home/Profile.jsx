20bda6b5670a6fed6c622bfb9eb42e16260af618
Moran <thaler.moran@gmail.com>
2022-12-06 15:12:53 +0100

    added the type animation package

--- src/components/home/Profile.jsx
+++ src/components/home/Profile.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { TypeAnimation } from 'react-type-animation';
 import "./profile.css"
 
 import { VscGithub } from 'react-icons/vsc';
@@ -23,6 +25,38 @@ function Profile() {
               <AiFillInstagram />
             </a>
           </div>
+
+          <div className="profile-details-name">
+            <span className='primary-text'>
+              {" "} Hello, I'M <span className='highlighted-text'>Moran</span>
+            </span>
+          </div>
+
+          <div className="profile-details-role">
+            <span className='primary-text'>
+              <span className='primary-text'>
+                {" "}
+                <h1>
+                  {" "}
+                  <TypeAnimation
+                    sequence={[
+                      '$ Hallo World ',
+                      2000, // Waits 2s
+                      'My name is Moran', // Deletes  and types
+                      2000, // Waits 2s
+                      "And I'm Front-end Developer",
+                      3000,
+                    ]}
+                    wrapper="div"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{ fontSize: '2em' }}
+                  />
+                </h1>
+              </span>
+            </span>
+          </div>
+
         </div>
       </div>
     </div>
